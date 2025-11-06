36013e6777263a628b36ab0d984bebb1d882525b
drop2653 <30587523+drop2653@users.noreply.github.com>
2025-11-06 16:18:21 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,36 +1,49 @@
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
-const PORT = process.env.PORT || 3000;
 const io = require('socket.io')(http);
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
 let players = {};
+let bullets = [];
 
 io.on('connection', socket => {
-    console.log('User connected:', socket.id);
-    
-    if (Object.keys(players).length < 2) {
-        players[socket.id] = {
-            id: socket.id,
-            x: 0,
-            y: 0
-        };
-        socket.emit('currentPlayers', players);
-        socket.broadcast.emit('newPlayer', players[socket.id]);
-    }
+    console.log('Connected:', socket.id);
+    players[socket.id] = { x: 200, y: 200 };
 
-    socket.on('shoot', (data) => {
-        socket.broadcast.emit('enemyShot', data);
+    socket.emit('init', socket.id);
+
+    socket.on('move', pos => {
+        if (players[socket.id]) {
+            players[socket.id] = pos;
+        }
+    });
+
+    socket.on('shoot', bullet => {
+        bullets.push({ ...bullet, speed: 6 });
     });
 
     socket.on('disconnect', () => {
-        console.log('User disconnected:', socket.id);
         delete players[socket.id];
-        io.emit('playerDisconnected', socket.id);
     });
 });
+
+// 매 30fps로 상태 동기화
+setInterval(() => {
+    // 총알 이동 처리
+    bullets.forEach(b => {
+        b.x += Math.cos(b.angle) * b.speed;
+        b.y += Math.sin(b.angle) * b.speed;
+    });
+
+    // 총알 제한
+    bullets = bullets.filter(b => b.x >= 0 && b.x <= 1920 && b.y >= 0 && b.y <= 1080);
+
+    io.emit('updatePlayers', players);
+    io.emit('updateBullets', bullets);
+}, 1000 / 30);
 
 http.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
